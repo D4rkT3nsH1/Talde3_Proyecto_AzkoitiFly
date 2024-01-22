bbc6cc2f9fb90b93e2b0c0ba6af564d042c6d679
ikerElorri <pacoustencio@gmail.com>
2024-01-22 09:48:37 +0100

    login

---
+++ controller/cLogin.php
@@ -0,0 +1,36 @@
+<?php
+include_once '../model/usuario_model.php';
+
+$User_Model = new usuario_model();
+
+// Inicializar la sesión
+session_start();
+
+try {
+    // Recibe los datos JSON
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    // Extrae los datos
+    $correo = $data['gmail_json'];
+    $pass = $data['pass_json'];
+
+    // Intenta realizar la inserción
+    $result = $User_Model->CheckUserLogin($correo, $pass);
+
+    if ($result) {
+        // Metemos los datos en la sesión
+        $_SESSION['correo'] = $correo;
+        $_SESSION['user'] = $result["nameUsuario"]; // usuario
+        $_SESSION['userId'] = $result["idUsuario"]; // idUsuario
+    }
+    
+    // Retorna un mensaje JSON según el resultado
+    if ($result) {
+        echo json_encode(["success" => true, "message" => "Te damos la bienvenida!", "usuario" => $result["nameUsuario"], "correoUsuario" => $result["correoUsuario"]]);
+    } else {
+        echo json_encode(["success" => false, "message" => "Correo o contraseña incorrectos"]);
+    }
+} catch (Exception $e) {
+    // Captura cualquier excepción y retorna un mensaje de error
+    echo json_encode(["success" => false, "message" => "Error en el servidor: " . $e->getMessage()]);
+}
---
+++ model/usuario_model.php
@@ -0,0 +1,70 @@
+<?php
+require_once 'connect_data.php';
+
+class usuario_model
+{
+    private $conn;
+
+    public function OpenConnect()
+    {
+        $connData = new connect_data();
+        try {
+            $this->conn = new mysqli($connData->host, $connData->userbbdd, $connData->passbbdd, $connData->ddbbname);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+        $this->conn->set_charset("utf8");
+    }
+
+    public function CloseConnect()
+    {
+        $this->conn = null;
+    }
+
+    public function UserByCorreo($correo)
+    {
+        $this->OpenConnect();
+
+        $sql = "SELECT * FROM usuarios WHERE correoUsuario = '$correo'";
+        $result = $this->conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $usuario_array = array();
+            $usuario_array['correoUsuario'] = $row['correoUsuario'];
+            $usuario_array['nameUsuario'] = $row['nameUsuario'];
+            $usuario_array['passUsuario'] = $row['passUsuario'];
+            if ($this->conn !== null) {
+                $this->CloseConnect();
+            }
+            return $usuario_array;
+        } else {
+            return false;
+        }
+    }
+    
+    public function CheckUserLogin($correo, $pass)
+    {
+        $this->OpenConnect();
+
+        $sql = "SELECT * FROM usuarios WHERE correoUsuario = '$correo' AND passUsuario = '$pass'";
+        $result = $this->conn->query($sql);
+
+        $datosUser = array();
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc(); // Obtener la fila de la consulta
+
+            $datosUser["idUsuario"] = $row['idUsuario'];
+            $datosUser["correoUsuario"] = $row['correoUsuario'];
+            $datosUser["nameUsuario"] = $row['nameUsuario'];
+
+            if ($this->conn !== null) {
+                $this->CloseConnect();
+            }
+            return $datosUser;
+        } else {
+            return false;
+        }
+    }
+}
